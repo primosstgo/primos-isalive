c57dc355650b99ea40c76895c205c27fa607d28f
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2023-06-07 03:14:34 -0400

    container red or green visual status

--- src/components/container.js
+++ src/components/container.js
@@ -1,19 +1,23 @@
 import moment from 'moment';
 
-export default function Container({data}) {
+export default function Container({ data }) {
   let ports = []
   for (let [i, port] of data.ports.entries()) {
     if (i > 0)
       ports.push('\u00A0')
     ports.push(<a href={'http://prime.inf.santiago.usm.cl:' + port} target="_blank">{port}</a>)
   }
+
+  // Define CSS classes for the status icon
+  const statusIconClass = data.status === "running" ? "green-icon" : "red-icon";
+
   return (
-    <div class='container'>
-      <div class='grid'>
-        <span style={{fontSize: '1.5em'}}>{data.name}</span>
+    <div className='container'>
+      <div className='grid'>
+        <span style={{ fontSize: '1.5em' }}>{data.name}</span>
         <span>{ports}</span>
         <span><i>{data.status}</i>&nbsp;{moment(data.status_time).fromNow()}</span>
-        <span>icono</span>
+        <span className={statusIconClass}></span>
       </div>
     </div>
   )
